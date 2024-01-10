49ade9f98e12e765c975f9e6682d16e9c6610377
dreamingfree09 <kostasdnds@outlook.com>
2024-01-10 19:29:01 +0000

    finaly

--- js/Home-script.js
+++ js/Home-script.js
@@ -1,4 +1,73 @@
 document.addEventListener('DOMContentLoaded', (event) => {
+
+    // Function to validate the username
+    window.validateUsername = function() {
+        const username = document.getElementById('username');
+        const errorDiv = document.getElementById('username-error');
+        if(username.value.length < 6 || username.value.length > 20) {
+            errorDiv.textContent = 'Username must be between 6 and 20 characters.';
+        } else {
+            errorDiv.textContent = '';
+        }
+    };
+
+    // Function to validate the email
+    window.validateEmail = function() {
+        const email = document.getElementById('email');
+        const errorDiv = document.getElementById('email-error');
+        const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+        if(!regex.test(email.value)) {
+            errorDiv.textContent = 'Invalid email format.';
+        } else {
+            errorDiv.textContent = '';
+        }
+    };
+
+    // Function to validate the password
+    window.validatePassword = function() {
+        const password = document.getElementById('password');
+        const errorDiv = document.getElementById('password-error');
+        if(password.value.length < 8 || password.value.length > 20) {
+            errorDiv.textContent = 'Password must be between 8 and 20 characters.';
+        } else {
+            errorDiv.textContent = '';
+        }
+    };
+
+    // Function to validate the confirm password
+    window.validateConfirmPassword = function() {
+        const password = document.getElementById('password');
+        const confirmPassword = document.getElementById('confirm-password');
+        const errorDiv = document.getElementById('confirm-password-error');
+        if(confirmPassword.value !== password.value) {
+            errorDiv.textContent = 'Passwords do not match.';
+        } else {
+            errorDiv.textContent = '';
+        }
+    };
+
+     // Function to validate the email in the login form
+    window.validateLoginEmail = function() {
+        const email = document.getElementById('login-email');
+        const errorDiv = document.getElementById('login-email-error');
+        const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+        if(!regex.test(email.value)) {
+            errorDiv.textContent = 'Invalid email format.';
+        } else {
+            errorDiv.textContent = '';
+        }
+    };
+
+    // Function to validate the password in the login form
+    window.validateLoginPassword = function() {
+        const password = document.getElementById('login-password');
+        const errorDiv = document.getElementById('login-password-error');
+        if(password.value.length < 8) {
+            errorDiv.textContent = 'Password must be at least 8 characters.';
+        } else {
+            errorDiv.textContent = '';
+        }
+    };
     // Set the current year in the footer
     document.getElementById("currentYear").textContent = new Date().getFullYear();
 
@@ -44,10 +113,12 @@ document.addEventListener('DOMContentLoaded', (event) => {
     setInterval(() => {
         changeImage(1);
     }, 3000); // Change image every 3000 milliseconds (3 seconds)
+
+
 });
 
-
-    const quizQuestions = [
+// All the questions and options
+const quizQuestions = [
     {
         id: 1,
         question: "Who won the FIFA World Cup in 2018?",
@@ -129,7 +200,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
 ];
 
 // Shuffle and select 10 random questions with shuffled options
-    function selectRandomQuestions(questions, count) {
+function selectRandomQuestions(questions, count) {
     shuffleArray(questions);
     return questions.slice(0, count).map(question => {
         let shuffledOptions = shuffleArray([...question.options]);
@@ -138,30 +209,31 @@ document.addEventListener('DOMContentLoaded', (event) => {
 }
 
 
-    function shuffleArray(array) {
-        for (let i = array.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]];
-        }
-        return array;
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
     }
+    return array;
+}
 
-    let currentQuestionIndex = 0;
-    let selectedQuestions = selectRandomQuestions([...quizQuestions], 10);
+let currentQuestionIndex = 0;
+let selectedQuestions = selectRandomQuestions([...quizQuestions], 10);
 
-    let correctCount = 0;
-    let wrongCount = 0;
-    let incompleteCount = 0;
-    let timer;
+let correctCount = 0;
+let wrongCount = 0;
+let incompleteCount = 0;
+let timer;
 
-    function displayCurrentQuestion() {
-        clearInterval(timer);
-        const question = selectedQuestions[currentQuestionIndex];
-        const quizForm = document.getElementById('quizForm');
-        quizForm.innerHTML = `<div id="timer"></div>` + createQuestionHtml(question);
-        quizForm.innerHTML += `<button id="nextQuestion" disabled>Next Question</button>`;
-        startTimer();
-    }
+function displayCurrentQuestion() {
+    clearInterval(timer);
+    const question = selectedQuestions[currentQuestionIndex];
+    const quizForm = document.getElementById('quizForm');
+    quizForm.innerHTML = `<div id="timer"></div>` + createQuestionHtml(question);
+    quizForm.innerHTML += `<button id="nextQuestion" disabled>Next Question</button>`;
+    startTimer();
+    updateProgress();
+}
 
 // Function to create HTML for a single quiz question
 function createQuestionHtml(question) {
@@ -170,9 +242,10 @@ function createQuestionHtml(question) {
         <ul>`;
 
     question.shuffledOptions.forEach((option, index) => {
+        let optionId = `option-${question.id}-${index}`;
         htmlContent += `<li>
-            <input type="radio" name="answer-${question.id}" value="${index}" onchange="answerSelected(${index}, ${question.id})">
-            <label for="option-${question.id}-${index}">${option}</label>
+            <input type="radio" id="${optionId}" name="answer-${question.id}" value="${index}" onchange="answerSelected(${index}, ${question.id})">
+            <label for="${optionId}">${option}</label>
         </li>`;
     });
 
@@ -180,61 +253,93 @@ function createQuestionHtml(question) {
     return htmlContent;
 }
 
-    window.enableNextButton = function() {
-        document.getElementById('nextQuestion').disabled = false;
-    };
+
+window.enableNextButton = function() {
+    document.getElementById('nextQuestion').disabled = false;
+};
 
 // Function to handle answer selection
 window.answerSelected = function(selectedOptionIndex, questionId) {
     const question = selectedQuestions.find(q => q.id === questionId);
     clearInterval(timer);
 
+    const selectedOption = document.querySelector(`input[name='answer-${questionId}']:checked`);
+    const optionsList = document.querySelectorAll(`input[name='answer-${questionId}']`);
+    optionsList.forEach(option => option.disabled = true); // Disable all options
+
     if (question.correctAnswerIndex === selectedOptionIndex) {
         correctCount++;
+        selectedOption.parentElement.classList.add('correct-answer'); 
     } else {
         wrongCount++;
+        selectedOption.parentElement.classList.add('wrong-answer');
     }
+
     enableNextButton();
 };
 
 
-    function startTimer() {
-        let timeLeft = 15;
-        const timerElement = document.getElementById('timer');
+// The timer and its implementation
+function startTimer() {
+    let timeLeft = 15;
+    const timerElement = document.getElementById('timer');
+    timerElement.textContent = `Time left: ${timeLeft} seconds`;
+
+    timer = setInterval(() => {
+        timeLeft--;
         timerElement.textContent = `Time left: ${timeLeft} seconds`;
 
-        timer = setInterval(() => {
-            timeLeft--;
-            timerElement.textContent = `Time left: ${timeLeft} seconds`;
-
-            if (timeLeft <= 0) {
-                clearInterval(timer);
-                timerElement.textContent = 'Time is up!';
-                incompleteCount++;
-                goToNextQuestion();
-            }
-        }, 1000);
-    }
-
-    function goToNextQuestion() {
-        if (currentQuestionIndex < selectedQuestions.length - 1) {
-            currentQuestionIndex++;
-            displayCurrentQuestion();
-        } else {
+        if (timeLeft <= 0) {
             clearInterval(timer);
-            displayResults();
+            timerElement.textContent = 'Time is up!';
+            incompleteCount++;
+            goToNextQuestion();
         }
-    }
+    }, 1000);
+}
 
-    function displayResults() {
-        const quizForm = document.getElementById('quizForm');
-        quizForm.innerHTML = `<h3>Quiz Completed!</h3>
-            <p>Correct Answers: ${correctCount}</p>
-            <p>Wrong Answers: ${wrongCount}</p>
-            <p>Incomplete Answers: ${incompleteCount}</p>`;
+function goToNextQuestion() {
+    if (currentQuestionIndex < selectedQuestions.length - 1) {
+        currentQuestionIndex++;
+        displayCurrentQuestion();
+    } else {
+        clearInterval(timer);
+        displayResults();
     }
+}
+// To display results
+function displayResults() {
+    const quizForm = document.getElementById('quizForm');
+    quizForm.innerHTML = `<h3>Quiz Completed!</h3>
+        <p>Correct Answers: ${correctCount}</p>
+        <p>Wrong Answers: ${wrongCount}</p>
+        <p>Incomplete Answers: ${incompleteCount}</p>`;
+}
+// Starting the quiz
+function initializeQuiz() {
+    currentQuestionIndex = 0;
+    correctCount = 0;
+    wrongCount = 0;
+    incompleteCount = 0;
+    selectedQuestions = selectRandomQuestions([...quizQuestions], 10);
+    displayCurrentQuestion();
+    document.getElementById('quizForm').classList.remove('hidden');
+    document.getElementById('startQuiz').style.display = 'none';
+    document.getElementById('quiz-container').style.backgroundImage = 'none';
+    
 
-    function initializeQuiz() {
+
+}
+
+document.getElementById('startQuiz').addEventListener('click', initializeQuiz);
+
+
+document.getElementById('quizForm').addEventListener('click', (event) => {
+    if (event.target && event.target.id === 'nextQuestion') {
+        goToNextQuestion();
+    }
+    // Restart quiz function
+    function restartQuiz() {
         currentQuestionIndex = 0;
         correctCount = 0;
         wrongCount = 0;
@@ -243,14 +348,12 @@ window.answerSelected = function(selectedOptionIndex, questionId) {
         displayCurrentQuestion();
         document.getElementById('quizForm').classList.remove('hidden');
         document.getElementById('startQuiz').style.display = 'none';
-        document.getElementById('quiz-container').style.backgroundImage = 'none';
+    }
+    //Update progress quiz
+    function updateProgress() {
+        const progressElement = document.getElementById('quizProgress');
+        progressElement.textContent = `Question ${currentQuestionIndex + 1} of ${selectedQuestions.length}`;
     }
 
-    document.getElementById('startQuiz').addEventListener('click', initializeQuiz);
 
-    document.getElementById('quizForm').addEventListener('click', (event) => {
-        if (event.target && event.target.id === 'nextQuestion') {
-            goToNextQuestion();
-        }
-    });
-;
+});
